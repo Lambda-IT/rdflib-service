190c7edc6cbaca0f997f0b873817dc8a1be2e020
Marc Berchtold <marc.berchtold@hivenet.ch>
2017-03-09 01:57:14 +0100

    Add complex xmp test data

--- src/RdfLibConverter.py
+++ src/RdfLibConverter.py
@@ -1,5 +1,6 @@
+
 import sys
-import rdflib 
+import rdflib
 
 from rdflib import Graph, plugin
 from rdflib.serializer import Serializer
@@ -11,7 +12,20 @@ def main():
 
     g = Graph().parse(data=source, format=sourceFormat)
 
-    print g.serialize(destination=None, format=targetFormat)
+    targetString = g.serialize(destination=None, format=targetFormat)
+
+    print(targetString)
+
+# def mainTest():
+#     source = "<rdf:RDF xmlns:rdf='http://www.w3.org/1999/02/22-rdf-syntax-ns#'><rdf:Description rdf:about='' xmlns:dc='http://purl.org/dc/elements/1.1/'><dc:subject><rdf:Bag><rdf:li>XMP</rdf:li><rdf:li>SDK</rdf:li><rdf:li>Test2</rdf:li></rdf:Bag></dc:subject><dc:format>image/tiff</dc:format></rdf:Description></rdf:RDF>"
+#     sourceFormat = "xml"
+#     targetFormat = "n3"
+
+#     g = Graph().parse(data=source, format=sourceFormat)
+
+#     targetString = g.serialize(destination=None, format=targetFormat)
+
+#     print(targetString)
 
 # Start process
 if __name__ == '__main__':
